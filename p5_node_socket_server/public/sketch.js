7c8a907e27131997a0703161fc1ef6e5d318a1c0
giy <giy.hands@gmail.com>
2018-09-23 23:20:53 +0900

    Text ball

--- p5_node_socket_server/public/sketch.js
+++ p5_node_socket_server/public/sketch.js
@@ -25,6 +25,8 @@ var drawImage = true;
 var img_random_idx = 0;
 
 
+var sentance = [];
+
 function setup() {
 
   createCanvas(innerWidth, innerHeight); // for display graph
@@ -38,6 +40,8 @@ function setup() {
       img[i] = loadImage("assets/" + i + ".jpg");
   }
 //   img = loadImage("assets/moonwalk.jpg");  // Load the image
+
+  sentance[0] = "폭탄이 비행기에서 풀리면 비행기의 수평 속도와 같은 수평 속도로 포물선 궤도를 따라 간다. 폭탄은 비행기 바로 밑으로 떨어집니다 (파선). 폭탄이 땅 위의 어떤 높이에서 폭발하면 조각의 질량 중심은 원래의 포물선 궤도 (주황색 곡선)를 따른다. 운동량은 보존됩니다. 즉, 폭발 직전의 각 조각에 대한 매스의 곱과 벡터의 합은 폭발 직후의 운동량과 같습니다";
 
 
 }
@@ -108,6 +112,53 @@ function draw() {
     // fill(255, 0, 0, al);
     // ellipse(width/2, height/2, 400, 400);
 
+    // text ball
+    push();
+    translate(innerWidth/2, innerHeight/2);
+    var r = frameCount*10 % 360;
+    rotate(r);
+    // console.log(r);
+
+    doDraw = true;
+    background(0);
+
+    var max = noise(frameCount/20) * 600;
+    var ka = 134 + noise(frameCount/50) * 1.7;
+    var space = 3 + 1 + noise(frameCount/30) * 4;
+    var ts = 30 + noise(frameCount/10) * 20;
+
+    if (doDraw) {
+
+        // fill(155, 100 * cos(a/3) * 255, 200);
+        for (var i = 0; i < max; i+=1) {
+
+            // var a = i * 135.6;
+            var a = i * ka;
+            var r = c * sqrt(i);
+
+            var x = space * r * cos(a); 
+            var y = space * r * sin(a);
+
+            var curChar = sentance[0][i%sentance[0].length];
+            // console.log(curChar);
+
+            fill(155, 100 * cos(a/3) * 255, 10 + 100 * ((max - i)/max));
+            textSize(ts);
+
+            if (curChar === '폭' || curChar === '탄') {
+                fill(5, 100, 10 + 100 * ((max - i)/max));
+                textSize(ts*2);
+            };
+
+            text(curChar, x, y);
+            // vertex(x, y);
+    
+        }
+ 
+    }
+
+
+
     // botany
     if (drawBotany == 1) {
         colorMode(HSB);
@@ -138,7 +189,17 @@ function draw() {
         noStroke();
             
     //    if (n % 3 == 0) {
-           ellipse(x, y, random(4, 8), random(4, 10));    
+            // ellipse(x, y, random(4, 8), random(4, 10));    
+
+            var curChar = sentance[0][n%sentance[0].length];
+            if (curChar === '폭' || curChar === '탄') {
+                fill(5, 100, 100);
+                textSize(20);
+            };
+
+
+            text(curChar, x, y);
+
     //    }
         n+=1;
     } else {
