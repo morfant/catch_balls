c4729e4132d7f6d5dd3dae8366809bdc0088eaf5
giy <giy.hands@gmail.com>
2018-09-28 16:47:16 +0900

    ..

--- p5_node_socket_server/app.js
+++ p5_node_socket_server/app.js
@@ -275,10 +275,10 @@ io.on('connection',  function(socket) {
         if (isStop[ballID] && !isRotating[ballID]) {
             isFlying[ballID] = false;
 
-            if (!isPrint_stopped){
+            if (!isPrint_stopped[ballID]){
                 console.log("ball " + ballID + " is stopped!!!!")
-                isPrint_stopped = true;
-                isPrint_not_stopped = false;
+                isPrint_stopped[ballID] = true;
+                isPrint_not_stopped[ballID] = false;
             }
 
             // osc to supercollider
@@ -376,10 +376,10 @@ io.on('connection',  function(socket) {
         } else {
 
             isFlying[ballID] = true;
-            if (!isPrint_not_stopped){
+            if (!isPrint_not_stopped[ballID]){
                 console.log("ball " + ballID + " is NOT stopped!!");
-                isPrint_not_stopped = true;
-                isPrint_stopped = false;
+                isPrint_not_stopped[ballID] = true;
+                isPrint_stopped[ballID] = false;
             }
 
             // SOUND
